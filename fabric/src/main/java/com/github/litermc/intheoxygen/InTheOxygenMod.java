97c88945d573b53bdcbe123ad24cdee256db1a75
zyxkad <zyxkad@gmail.com>
2025-06-02 17:07:12 -0600

    first commit

---
+++ fabric/src/main/java/com/github/litermc/intheoxygen/InTheOxygenMod.java
@@ -0,0 +1,9 @@
+package com.github.litermc.intheoxygen;
+
+import net.fabricmc.api.ModInitializer;
+
+public class InTheOxygenMod implements ModInitializer {
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ forge/src/main/java/com/github/litermc/intheoxygen/InTheOxygenMod.java
@@ -0,0 +1,9 @@
+package com.github.litermc.intheoxygen;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Constants.MOD_ID)
+public class InTheOxygenMod {
+	public InTheOxygenMod() {
+	}
+}
